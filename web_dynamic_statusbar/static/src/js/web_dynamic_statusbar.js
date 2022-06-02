99260af0a6afd6c28674c8cce9edec02fbcf598e
Stefan Rijnhart <stefan@opener.am>
2022-06-02 16:37:02 +0200

    [ADD] web_dynamic_statusbar

---
+++ web_dynamic_statusbar/static/src/js/web_dynamic_statusbar.js
@@ -0,0 +1,25 @@
+/* Copyright 2022 Stefan Rijnhart <stefan@opener.amsterdam>
+   License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
+odoo.define("web_dynamic_statusbar", function (require) {
+    "use strict";
+    var FieldStatus = require("web.relational_fields").FieldStatus;
+    var DynamicFieldStatus = FieldStatus.include({
+        _setState: function () {
+            this._super();
+            const visible_states_string = this.recordData[
+                this.name + "_visible_states"
+            ];
+            if (visible_states_string) {
+                const visible_states = visible_states_string.split(",");
+                this.status_information = this.status_information.filter(function (
+                    value,
+                    index, // eslint-disable-line no-unused-vars
+                    arr // eslint-disable-line no-unused-vars
+                ) {
+                    return value.selected || visible_states.includes(String(value.id));
+                });
+            }
+        },
+    });
+    return DynamicFieldStatus;
+});
